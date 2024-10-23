130dc4dcbdb22b30878c9e38a295574e41689f23
Maria Gavrilina <maria.gavrilina@2vmdls.com>
2024-10-23 16:21:56 +0300

    fix: update analysis_options and associated code

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:memo/colors.dart';
 import 'package:memo/memorisation_model.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
-const String memoryBoxName = "MemorisationBox";
+const String memoryBoxName = 'MemorisationBox';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final document = await getApplicationDocumentsDirectory();
-  Hive.init(document.path);
-  Hive.registerAdapter(MemorisationModelAdapter());
+  Hive
+    ..init(document.path)
+    ..registerAdapter(MemorisationModelAdapter());
   await Hive.openBox<MemorisationModel>(memoryBoxName);
   runApp(const MyApp());
 }
@@ -20,10 +21,9 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
+  Widget build(final BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
           textTheme: GoogleFonts.caveatTextTheme(Theme.of(context).textTheme),
           inputDecorationTheme: InputDecorationTheme(focusColor: green),
           appBarTheme: AppBarTheme(backgroundColor: green, shadowColor: milk),
@@ -32,10 +32,10 @@ class MyApp extends StatelessWidget {
               FloatingActionButtonThemeData(backgroundColor: red),
           dialogTheme: DialogTheme(
             backgroundColor: milk,
-          )),
-      home: const MyHomePage(),
-    );
-  }
+          ),
+        ),
+        home: const MyHomePage(),
+      );
 }
 
 class MyHomePage extends StatefulWidget {
@@ -45,7 +45,11 @@ class MyHomePage extends StatefulWidget {
   MyHomePageState createState() => MyHomePageState();
 }
 
-enum Filter { all, remembered, unremembered }
+enum Filter {
+  all,
+  remembered,
+  unremembered,
+}
 
 class MyHomePageState extends State<MyHomePage> {
   late Box<MemorisationModel> memoryBox;
@@ -63,117 +67,124 @@ class MyHomePageState extends State<MyHomePage> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text(
-            // 6 steps for Android
-            "      Mémo",
-            style: TextStyle(fontSize: 30),
+  Widget build(final BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text(
+              // 6 steps for Android
+              '      Mémo',
+              style: TextStyle(fontSize: 30),
+            ),
           ),
-        ),
-        actions: <Widget>[
-          PopupMenuButton<String>(
-            icon: const Padding(
+          actions: <Widget>[
+            PopupMenuButton<String>(
+              icon: const Padding(
                 padding: EdgeInsets.only(right: 15),
                 child: Icon(
                   Icons.arrow_drop_down,
-                )),
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(20.0),
+                ),
               ),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+              color: milk,
+              onSelected: (final value) {
+                if (value.compareTo('Tous') == 0) {
+                  setState(() {
+                    filter = Filter.all;
+                  });
+                } else if (value.compareTo('Mémorisé') == 0) {
+                  setState(() {
+                    filter = Filter.remembered;
+                  });
+                } else {
+                  setState(() {
+                    filter = Filter.unremembered;
+                  });
+                }
+              },
+              itemBuilder: (final context) => [
+                'Tous',
+                'Mémorisé',
+                'Non mémorisé',
+              ]
+                  .map(
+                    (final option) => PopupMenuItem(
+                      textStyle: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontFamily: 'Caveat',
+                      ),
+                      value: option,
+                      child: Text(option),
+                    ),
+                  )
+                  .toList(),
             ),
-            color: milk,
-            onSelected: (value) {
-              if (value.compareTo('Tous') == 0) {
-                setState(() {
-                  filter = Filter.all;
-                });
-              } else if (value.compareTo('Mémorisé') == 0) {
-                setState(() {
-                  filter = Filter.remembered;
-                });
-              } else {
-                setState(() {
-                  filter = Filter.unremembered;
-                });
-              }
-            },
-            itemBuilder: (BuildContext context) {
-              return [
-                "Tous",
-                "Mémorisé",
-                "Non mémorisé",
-              ].map((option) {
-                return PopupMenuItem(
-                  textStyle: const TextStyle(
-                      fontSize: 20, color: Colors.black, fontFamily: 'Caveat'),
-                  value: option,
-                  child: Text(option),
-                );
-              }).toList();
-            },
-          )
-        ],
-      ),
-      body: ValueListenableBuilder(
-        valueListenable: memoryBox.listenable(),
-        builder: (context, Box<MemorisationModel> words, _) {
-          List<int> keys;
+          ],
+        ),
+        body: ValueListenableBuilder(
+          valueListenable: memoryBox.listenable(),
+          builder: (final context, final words, final _) {
+            List<int> keys;
 
-          if (filter == Filter.all) {
-            keys = words.keys.cast<int>().toList();
-          } else if (filter == Filter.remembered) {
-            keys = words.keys
-                .cast<int>()
-                .where((key) => words.get(key)!.isLearned)
-                .toList();
-          } else {
-            keys = words.keys
-                .cast<int>()
-                .where((key) => !words.get(key)!.isLearned)
-                .toList();
-          }
+            if (filter == Filter.all) {
+              keys = words.keys.cast<int>().toList();
+            } else if (filter == Filter.remembered) {
+              keys = words.keys
+                  .cast<int>()
+                  .where((final key) => words.get(key)!.isLearned)
+                  .toList();
+            } else {
+              keys = words.keys
+                  .cast<int>()
+                  .where((final key) => !words.get(key)!.isLearned)
+                  .toList();
+            }
 
-          return ListView.separated(
-            itemBuilder: (_, index) {
-              final int key = keys[index];
-              final MemorisationModel? memorisation = words.get(key);
-              return ListTile(
-                contentPadding: const EdgeInsets.only(left: 10.0, right: 5.0),
-                minLeadingWidth: 0.0,
-                minVerticalPadding: 0.0,
-                title: Transform.translate(
-                  offset: const Offset(0.0, 1.0),
-                  child: Text(
-                    memorisation!.word,
-                    style: const TextStyle(fontSize: 35),
+            return ListView.separated(
+              itemBuilder: (final _, final index) {
+                final int key = keys[index];
+                final MemorisationModel? memorisation = words.get(key);
+                return ListTile(
+                  contentPadding: const EdgeInsets.only(left: 10, right: 5),
+                  minLeadingWidth: 0,
+                  minVerticalPadding: 0,
+                  title: Transform.translate(
+                    offset: const Offset(0, 1),
+                    child: Text(
+                      memorisation!.word,
+                      style: const TextStyle(fontSize: 35),
+                    ),
                   ),
-                ),
-                subtitle: Transform.translate(
-                  offset: const Offset(0.0, -5.0),
-                  child: Text('[ ${memorisation.transcription} ]',
-                      style: const TextStyle(fontSize: 26)),
-                ),
-                leading: IconButton(
+                  subtitle: Transform.translate(
+                    offset: const Offset(0, -5),
+                    child: Text(
+                      '[ ${memorisation.transcription} ]',
+                      style: const TextStyle(fontSize: 26),
+                    ),
+                  ),
+                  leading: IconButton(
                     onPressed: () {
-                      setState(() {
+                      setState(() async {
                         if (!memorisation.isLearned) {
-                          MemorisationModel mTodo = MemorisationModel(
-                              word: memorisation.word,
-                              transcription: memorisation.transcription,
-                              translation: memorisation.translation,
-                              isLearned: true);
-                          memoryBox.put(key, mTodo);
+                          final MemorisationModel mTodo = MemorisationModel(
+                            word: memorisation.word,
+                            transcription: memorisation.transcription,
+                            translation: memorisation.translation,
+                            isLearned: true,
+                          );
+                          await memoryBox.put(key, mTodo);
                         } else {
-                          MemorisationModel mTodo = MemorisationModel(
-                              word: memorisation.word,
-                              transcription: memorisation.transcription,
-                              translation: memorisation.translation,
-                              isLearned: false);
-                          memoryBox.put(key, mTodo);
+                          final MemorisationModel mTodo = MemorisationModel(
+                            word: memorisation.word,
+                            transcription: memorisation.transcription,
+                            translation: memorisation.translation,
+                            isLearned: false,
+                          );
+                          await memoryBox.put(key, mTodo);
                         }
                       });
                     },
@@ -183,43 +194,49 @@ class MyHomePageState extends State<MyHomePage> {
                           ? Colors.amber
                           : Colors.grey.shade600,
                       size: 32,
-                    )),
-                trailing: IconButton(
+                    ),
+                  ),
+                  trailing: IconButton(
                     icon: const Icon(Icons.clear),
                     color: red,
                     onPressed: () {
-                      setState(() {
-                        memoryBox.deleteAt(index);
-                        memoryBox.compact();
+                      setState(() async {
+                        await memoryBox.deleteAt(index);
+                        await memoryBox.compact();
                       });
-                    }),
-                onTap: () {
-                  showDialog(
+                    },
+                  ),
+                  onTap: () async {
+                    await showDialog(
                       context: context,
-                      builder: (context) => Dialog(
-                          child: Container(
-                              padding: const EdgeInsets.all(10),
-                              child: Text(
-                                memorisation.translation,
-                                textAlign: TextAlign.center,
-                                style: const TextStyle(fontSize: 30),
-                              ))));
-                },
-              );
-            },
-            separatorBuilder: (_, index) => const Divider(),
-            itemCount: keys.length,
-            shrinkWrap: true,
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) => Dialog(
+                      builder: (final context) => Dialog(
+                        child: Container(
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            memorisation.translation,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(fontSize: 30),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+              separatorBuilder: (final _, final index) => const Divider(),
+              itemCount: keys.length,
+              shrinkWrap: true,
+            );
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            await showDialog(
+              context: context,
+              builder: (final context) => Dialog(
                 shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
                 child: Container(
                   padding: const EdgeInsets.all(16),
                   child: Column(
@@ -229,11 +246,13 @@ class MyHomePageState extends State<MyHomePage> {
                         cursorColor: teal,
                         style: const TextStyle(fontSize: 28),
                         decoration: InputDecoration(
-                            focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(color: teal)),
-                            hintText: "Un mot",
-                            hintStyle: const TextStyle(fontSize: 24),
-                            focusColor: Colors.grey),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: teal),
+                          ),
+                          hintText: 'Un mot',
+                          hintStyle: const TextStyle(fontSize: 24),
+                          focusColor: Colors.grey,
+                        ),
                         controller: wordController,
                       ),
                       const SizedBox(
@@ -244,8 +263,9 @@ class MyHomePageState extends State<MyHomePage> {
                         style: const TextStyle(fontSize: 28),
                         decoration: InputDecoration(
                           focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: teal)),
-                          hintText: "Une transcription",
+                            borderSide: BorderSide(color: teal),
+                          ),
+                          hintText: 'Une transcription',
                           hintStyle: const TextStyle(fontSize: 24),
                         ),
                         controller: transcriptionController,
@@ -257,10 +277,11 @@ class MyHomePageState extends State<MyHomePage> {
                         cursorColor: teal,
                         style: const TextStyle(fontSize: 28),
                         decoration: InputDecoration(
-                          hintText: "Une traduction",
+                          hintText: 'Une traduction',
                           hintStyle: const TextStyle(fontSize: 24),
                           focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: teal)),
+                            borderSide: BorderSide(color: teal),
+                          ),
                         ),
                         controller: translationController,
                       ),
@@ -275,11 +296,12 @@ class MyHomePageState extends State<MyHomePage> {
                           final String translationText =
                               translationController.text;
 
-                          MemorisationModel todo = MemorisationModel(
-                              word: wordText,
-                              transcription: transcriptionText,
-                              translation: translationText,
-                              isLearned: false);
+                          final MemorisationModel todo = MemorisationModel(
+                            word: wordText,
+                            transcription: transcriptionText,
+                            translation: translationText,
+                            isLearned: false,
+                          );
 
                           memoryBox.add(todo);
                           wordController.clear();
@@ -289,27 +311,30 @@ class MyHomePageState extends State<MyHomePage> {
                           Navigator.pop(context);
                         },
                         style: ButtonStyle(
-                            backgroundColor: WidgetStateProperty.all(teal),
-                            shape: WidgetStateProperty.all(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10)))),
+                          backgroundColor: WidgetStateProperty.all(teal),
+                          shape: WidgetStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
                         child: Text(
-                          "Ajouter un nouveau mot",
+                          'Ajouter un nouveau mot',
                           style: TextStyle(
                             fontSize: 22,
                             color: milk,
                           ),
                         ),
-                      )
+                      ),
                     ],
                   ),
-                )),
-          );
-        },
-        child: const Icon(
-          Icons.add,
+                ),
+              ),
+            );
+          },
+          child: const Icon(
+            Icons.add,
+          ),
         ),
-      ),
-    );
-  }
+      );
 }
--- lib/memorisation_model.dart
+++ lib/memorisation_model.dart
@@ -4,6 +4,12 @@ part 'memorisation_model.g.dart';
 
 @HiveType(typeId: 0)
 class MemorisationModel extends HiveObject {
+  MemorisationModel({
+    required this.word,
+    required this.transcription,
+    required this.isLearned,
+    required this.translation,
+  });
   @HiveField(0)
   final String word;
   @HiveField(1)
@@ -12,10 +18,4 @@ class MemorisationModel extends HiveObject {
   final bool isLearned;
   @HiveField(3)
   final String translation;
-
-  MemorisationModel(
-      {required this.word,
-      required this.transcription,
-      required this.isLearned,
-      required this.translation});
 }
